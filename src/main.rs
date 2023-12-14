f22450682d45c0a8e642cf1a09e0e5a558612cba
Sascha Lüdecke <sascha@meta-x.de>
2023-12-14 21:41:13 +0100

    Started implementing the API: confirmation and alert
    . message, title and dialog type are now passed to the Dialog application as
      DialogFlags
    . UI is adjusted accordingly
    . API functions confirmation(...) and alert(...) added

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,44 @@ use std::sync::Mutex;
 use gilrs::{Button, Event, Gilrs};
 use iced::futures::SinkExt;
 use iced::widget::{button, column, container, row, text};
-use iced::{
-    executor, subscription, Alignment, Application, Command, Element, Length, Settings, Theme,
-};
+use iced::{executor, subscription, Application, Command, Element, Length, Settings, Theme};
 use iced::{window, Subscription};
 use icons::Icon;
 
 mod icons;
 
-struct Dialog {
-    has_gamepad: bool,
+/// The result of the user interaction
+#[derive(Debug, Copy, Clone)]
+pub enum DialogResult {
+    Undefined,
+    Cancel,
+    OK,
 }
 
+struct Dialog {
+    has_gamepad: bool,
+    flags: DialogFlags,
+}
+
+/// Type of dialog, either confirmation or alert
+#[derive(Default, PartialEq)]
+enum DialogType {
+    #[default]
+    Confirmation,
+    Alert,
+}
+
+/// Configuration flags for the dialog
+#[derive(Default)]
+struct DialogFlags {
+    title: String,
+    message: String,
+    dialog_type: DialogType,
+}
+
+/// Messages to be passed within the dialog
 #[derive(Debug, Clone, Copy)]
-pub enum Message {
+enum Message {
     FontLoaded,
     CancelPressed,
     OKPressed,
@@ -25,12 +49,7 @@ pub enum Message {
     GamepadDisconnected,
 }
 
-#[derive(Debug)]
-pub enum DialogResult {
-    Cancel,
-    OK,
-}
-
+/// Gilrs loop to capture gamepad events as subscriptions
 fn listen_to_gilrs() -> Subscription<Message> {
     struct Connect;
 
@@ -65,7 +84,7 @@ fn listen_to_gilrs() -> Subscription<Message> {
 
 // NOTE.2023-12-12 this is not really thread safe, since multiple dialogs from
 // different threads will try to modify this variable, though mutexed.
-pub static RR: Mutex<Option<DialogResult>> = Mutex::new(None);
+pub static DIALOG_RESULT_MUTEX: Mutex<DialogResult> = Mutex::new(DialogResult::Undefined);
 
 impl Application for Dialog {
     type Executor = executor::Default;
@@ -74,9 +93,9 @@ impl Application for Dialog {
 
     type Theme = Theme;
 
-    type Flags = ();
+    type Flags = DialogFlags;
 
-    fn new(_: Self::Flags) -> (Self, Command<Self::Message>) {
+    fn new(flags: Self::Flags) -> (Self, Command<Self::Message>) {
         (
             Self {
                 // NOTE.2023-12-13 to self: this is a way to find first connected gamepad or none
@@ -91,25 +110,27 @@ impl Application for Dialog {
                         }
                     })
                     .unwrap_or(false),
+                flags,
             },
             iced::font::load(icons::ICONS_DATA).map(|_| Message::FontLoaded),
         )
     }
 
     fn title(&self) -> String {
-        String::from("Ludusavi")
+        self.flags.title.clone()
     }
 
     fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
+        println!("Message received: {message:?}");
         match message {
             Message::CancelPressed | Message::GamepadButton(Button::East) => {
                 println!("Cancel pressed");
-                *RR.lock().unwrap() = Some(DialogResult::Cancel);
+                *DIALOG_RESULT_MUTEX.lock().unwrap() = DialogResult::Cancel;
                 window::close()
             }
             Message::OKPressed | Message::GamepadButton(Button::South) => {
                 println!("OK pressed");
-                *RR.lock().unwrap() = Some(DialogResult::OK);
+                *DIALOG_RESULT_MUTEX.lock().unwrap() = DialogResult::OK;
                 window::close()
             }
             Message::FontLoaded => {
@@ -132,37 +153,46 @@ impl Application for Dialog {
     }
 
     fn view(&self) -> Element<'_, Self::Message, iced::Renderer<Self::Theme>> {
-        // +------------------------------+
-        // |         Ludusavi             |
-        // |  ===                         |
-        // |   =     Message              |
-        // |  ===                         |
-        // |       [(B) Cancel] [(A) OK]  |
-        // +------------------------------+
+        // +--------------------------------+
+        // |         Ludusavi               |
+        // |  ===                           |
+        // |   =     Message                |
+        // |  ===                           |
+        // |         [(B) Cancel] [(A) OK]  |
+        // +--------------------------------+
 
         let label_cancel = match self.has_gamepad {
             true => row![Icon::XboxB.text_small(), "Cancel"].spacing(4),
             false => row!["Cancel"],
         };
+
         let label_ok = match self.has_gamepad {
             true => row![Icon::XboxA.text_small(), "OK"].spacing(4),
             false => row!["OK"],
         };
+
+        let mut button_row = row![]
+            .spacing(8)
+            .width(Length::Fill)
+            .align_items(iced::Alignment::Center);
+        if self.flags.dialog_type == DialogType::Confirmation {
+            button_row = button_row.push(button(label_cancel).on_press(Message::CancelPressed));
+        }
+        button_row = button_row.push(button(label_ok).on_press(Message::OKPressed));
+
+        let icon = match self.flags.dialog_type {
+            DialogType::Confirmation => Icon::InfoSquareRounded.text(),
+            DialogType::Alert => Icon::AlertSquareRounded.text(),
+        };
+
         container(
             row![
-                Icon::InfoSquareRounded.text(),
+                icon,
                 column![
-                    text("Testmessage")
+                    text(self.flags.message.clone())
                         .vertical_alignment(iced::alignment::Vertical::Top)
                         .height(Length::Fill),
-                    row![
-                        // normal buttons
-                        button(label_cancel).on_press(Message::CancelPressed),
-                        button(label_ok).on_press(Message::OKPressed),
-                    ]
-                    .spacing(8)
-                    .width(Length::Fill)
-                    .align_items(iced::Alignment::Center)
+                    button_row,
                 ]
                 .spacing(8),
             ]
@@ -182,23 +212,47 @@ impl Application for Dialog {
     }
 }
 
-// TODO.2023-12-12 implement
-// fn confirmation(title: &str, message: &str) -> Result<bool, Error> {}
-// fn alert(title: &str, message: &str) -> Result<(), Error> {}
+pub fn confirmation(title: &str, message: &str) -> Result<DialogResult, iced::Error> {
+    // TODO.2023-12-12 escape shall close window
+    Dialog::run(Settings {
+        window: window::Settings {
+            size: (300, 150),
+            ..Default::default()
+        },
+        exit_on_close_request: false,
+        flags: DialogFlags {
+            title: title.to_string(),
+            message: message.to_string(),
+            dialog_type: DialogType::Confirmation,
+        },
+        ..Default::default()
+    })?;
+
+    Ok(*DIALOG_RESULT_MUTEX.lock().unwrap())
+}
+
+pub fn alert(title: &str, message: &str) -> Result<DialogResult, iced::Error> {
+    // TODO.2023-12-12 escape shall close window
+    Dialog::run(Settings {
+        window: window::Settings {
+            size: (300, 150),
+            ..Default::default()
+        },
+        exit_on_close_request: false,
+        flags: DialogFlags {
+            title: title.to_string(),
+            message: message.to_string(),
+            dialog_type: DialogType::Alert,
+        },
+        ..Default::default()
+    })?;
+
+    Ok(*DIALOG_RESULT_MUTEX.lock().unwrap())
+}
 
 fn main() {
     println!("Hello, world!");
-    // TODO.2023-12-12 escape shall close window
-    println!(
-        "{:#?}",
-        Dialog::run(Settings {
-            window: window::Settings {
-                size: (300, 150),
-                ..Default::default()
-            },
-            exit_on_close_request: true,
-            ..Default::default()
-        })
-    );
-    println!("Goodbye world: {RR:#?}");
+    println!("{:?}", alert("Ludusavi", "Something went wrong!"));
+    println!("{:?}", confirmation("Ludusavi", "Confirmation requested"));
+    println!("Goodbye world!");
 }
